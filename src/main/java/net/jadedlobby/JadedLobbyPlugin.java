75218e942b42889845055c5a1c83f2fcd653a075
firewolf8385 <19617807+firewolf8385@users.noreply.github.com>
2023-12-27 22:04:56 -0500

    Initial commit

---
+++ src/main/java/net/jadedlobby/JadedLobbyPlugin.java
@@ -0,0 +1,12 @@
+package net.jadedlobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JadedLobbyPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
